7192b863c2ffefbf830bd27bf056c8d6b5a19970
Bianca <biancacmatira@gmail.com>
2020-05-27 10:59:19 -0700

    Done with edit and delete function

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+// const mongoConnect = require('./util/database').mongoConnect;
 // const User = require('./models/user');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use((req, res, next) => {
 //   User.findById('5ecd1f005bd4eba072d49802') //change the ID to a user id on your DB (create a users collection)
@@ -28,11 +29,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     .catch(err => console.log(err));
 // });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
